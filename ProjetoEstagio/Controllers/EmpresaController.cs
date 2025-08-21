c7e87fb71400d5221922e600d7daa4ea79020903
Robson Issomoto <robson.issomoto@fatec.sp.gov.br>
2025-08-21 00:38:18 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoEstagio/Controllers/EmpresaController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoEstagio.Data;
+using ProjetoEstagio.Models;
+
+namespace ProjetoEstagio.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmpresaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index(string cnpj = null)
+        {
+            Empresa model = new Empresa();
+
+            if (!string.IsNullOrEmpty(cnpj))
+            {
+                model = _context.empresas.FirstOrDefault(e => e.CNPJ == cnpj);
+                if (model == null)
+                    return NotFound();
+            }
+
+            //ViewBag.Empresas = _context.empresas.ToList();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Save(Empresa empresa, string action)
+        {
+            if (action == "cadastrar" && ModelState.IsValid)
+            {
+                _context.empresas.Add(empresa);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else if (action == "atualizar" && ModelState.IsValid)
+            {
+                _context.empresas.Update(empresa);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+
+            ViewBag.Empresas = _context.empresas.ToList();
+            return View("Index");
+        }
+
+        
+        [HttpPost]
+        
+        public IActionResult DeleteConfirmedPorCNPJ(string cnpj)
+        {
+            if (string.IsNullOrWhiteSpace(cnpj))
+                return BadRequest("CNPJ inválido.");
+
+            var empresa = _context.empresas.FirstOrDefault(e => e.CNPJ == cnpj);
+            if (empresa != null)
+            {
+                _context.empresas.Remove(empresa);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProjetoEstagio/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoEstagio.Models;
+
+namespace ProjetoEstagio.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
+        {
+        }
+        public DbSet<Empresa> empresas {  get; set; }
+    }
+}
